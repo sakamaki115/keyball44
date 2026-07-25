01b25fd0a5697f6fb15fe68da733f5602e879c78
ksdr <syameimaru4173@gmail.com>
2026-07-25 13:21:15 +0900

    sdf

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_copy/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_copy/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
